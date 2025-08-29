3b517758085448a5ba6a7def79a01e3e7874d954
Yasmin Ventura <yasmin.gomsv@gmail.com>
2025-08-29 14:37:44 -0300

    feat: add data access repositories

---
+++ github-profile-manager/src/main/java/com/github_profile_manager/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.github_profile_manager.repositories;
+
+import com.github_profile_manager.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
