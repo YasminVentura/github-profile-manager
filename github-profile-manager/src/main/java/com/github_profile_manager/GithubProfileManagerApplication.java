9b750a05d614075d4af1cab81ccccc8389114805
Yasmin Ventura <yasmin.gomsv@gmail.com>
2025-08-29 13:34:00 -0300

    first commit

---
+++ github-profile-manager/src/main/java/com/github_profile_manager/GithubProfileManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github_profile_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubProfileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubProfileManagerApplication.class, args);
+	}
+
+}
---
+++ github-profile-manager/src/test/java/com/github_profile_manager/GithubProfileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github_profile_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubProfileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
